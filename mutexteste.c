5067d886764edca0ba31cb328d119786502f5ebe
SamanthaBeatriz19 <49728936+SamanthaBeatriz19@users.noreply.github.com>
2019-11-01 20:46:08 -0300

    update

--- mutexteste.c
+++ mutexteste.c
@@ -3,8 +3,9 @@
 #include<pthread.h> 
 #include<stdlib.h> 
 #include<unistd.h> 
+//gcc *c -pthread
 
-pthread_t tid[2]; 
+pthread_t tid[5]; 
 int counter; 
 pthread_mutex_t lock; 
 
@@ -18,7 +19,7 @@ void* trythis(void *arg)
 	FILE * foo = fopen("teste.dat","r");
 	while(!feof(foo)){
 		fscanf(foo,"%d\n",&dado[a++]);
-		printf("num= %d\n",counter,dado[a-1]);
+		printf("num= %d\n",dado[a-1]);
 	}
 	fclose(foo);
 
@@ -30,26 +31,27 @@ void* trythis(void *arg)
 } 
 
 int main(int argc, char *argv[ ]) //os argumentos do main entram na linha do prompt
-{ 
+{ // o argume
 	int i = 0; 
 	int error; 
-
+	int numero = strtol (argv[1],NULL,10);
+	printf("-%d\n",numero);
 	if (pthread_mutex_init(&lock, NULL) != 0) 
 	{ 
-		printf("\n mutex não iniciou\n"); 
+		printf("\n mutex init has failed\n"); 
 		return 1; 
 	} 
 
-	while(i < 2) 
+	while(i < numero) 
 	{ 
 		error = pthread_create(&(tid[i]), NULL, &trythis, NULL); 
 		if (error != 0) 
-			printf("\nThread deu erro :[%s]", strerror(error)); 
+			printf("\nThread can't be created :[%s]", strerror(error)); 
 		i++; 
 	} 
-
-	pthread_join(tid[0], NULL); 
-	pthread_join(tid[1], NULL); 
+	for(int a=0;a<numero;a++){
+		pthread_join(tid[a], NULL); 
+	} 
 	pthread_mutex_destroy(&lock); 
 
 	return 0; 
