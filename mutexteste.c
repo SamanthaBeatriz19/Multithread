53871f092ee5723928bfeccde444a31ace99ee15
SamanthaBeatriz19 <49728936+SamanthaBeatriz19@users.noreply.github.com>
2019-11-07 16:22:44 -0300

    logic
    a logica ta aki ela so n funfa

--- mutexteste.c
+++ mutexteste.c
@@ -9,49 +9,100 @@ pthread_t tid[5];
 int counter; 
 pthread_mutex_t lock; 
 
-void* trythis(void *arg) 
+void merge(int *vetor,int *aux,int ini,int fim, int meio){
+	int iv=ini,ic=meio+1;
+	for(int a=ini;a<=fim;a++){
+		aux[a]=vetor[a];
+	}
+	printf("Merge\n");
+	int a=ini;
+	while((iv<=meio)&&ic<=fim){
+		if(aux[iv]<aux[ic])
+			vetor[a++]=aux[ic++];
+		else
+			vetor[a++]=aux[ic++];
+	}
+	while (iv <= meio) {
+		vetor[a++] = aux[iv++];	
+		printf("M %d\n",vetor[a]);
+	}
+	 while (ic <= fim) {
+		vetor[a++] = aux[ic++];
+	}
+}
+
+void sort(int *vetor,int *aux,int ini,int fim){
+	if(ini>=fim)
+		return;
+	int meio=(ini+fim)/2;
+	//a função chama a si mesma
+	sort(vetor,aux,ini,meio);
+	sort(vetor,aux,meio+1,fim);
+	
+	//if(vetor[meio]<=vetor[meio+1])
+		//return;
+	merge(vetor,aux,ini,fim,meio);
+}
+
+
+
+void* trythis(void *arr) 
 { 
 	pthread_mutex_lock(&lock); 
-
+	int *vetor = arr;
+	int total;
 	counter += 1; 
 	printf("\n Job %d has started\n", counter); 
-	int a=0,dado[20];
-	FILE * foo = fopen("teste.dat","r");
-	while(!feof(foo)){
-		fscanf(foo,"%d\n",&dado[a++]);
-		printf("num= %d\n",dado[a-1]);
-	}
-	fclose(foo);
-
+	//merge e sort tem q vir aki
+	total=sizeof(vetor)/sizeof(vetor[0]);
+	printf("total %d\n",total);
+	int *aux = malloc(sizeof(int) * total);
+	sort(vetor,aux,0,total-1);
+	free(aux);	
 	printf("\n Job %d has finished\n", counter); 
-
 	pthread_mutex_unlock(&lock); 
-
 	return NULL; 
 } 
 
+int array[10000];
+
 int main(int argc, char *argv[ ]) //os argumentos do main entram na linha do prompt
-{ // o argume
+{ 
 	int i = 0; 
 	int error; 
 	int numero = strtol (argv[1],NULL,10);
-	printf("-%d\n",numero);
+	printf("->%d\n",argc);
 	if (pthread_mutex_init(&lock, NULL) != 0) 
 	{ 
-		printf("\n mutex init has failed\n"); 
+		printf("\n mutex falhou\n"); 
 		return 1; 
 	} 
-
+	//abre os arquivos
+	int op=0;
+	if(argc>=2){
+		for(int a=2; a<argc;a++){
+			printf("argv %s\n",argv[a]);
+			FILE *fp = fopen(argv[a],"r");
+				while(!feof(fp)){
+				fscanf(fp,"%d\n",&array[op++]);
+				}
+			fclose(fp);
+		}
+	}	
+	
 	while(i < numero) 
 	{ 
-		error = pthread_create(&(tid[i]), NULL, &trythis, NULL); 
+		error = pthread_create(&(tid[i]), NULL, &trythis, &array); 
 		if (error != 0) 
-			printf("\nThread can't be created :[%s]", strerror(error)); 
+			printf("\nThread não criada:[%s]", strerror(error)); 
 		i++; 
 	} 
 	for(int a=0;a<numero;a++){
 		pthread_join(tid[a], NULL); 
-	} 
+	}
+	for(int a=0;a<op;a++){
+		printf("num= %d\n",array[a]);
+	}
 	pthread_mutex_destroy(&lock); 
 
 	return 0; 
